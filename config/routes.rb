d1aba8645d19383e419173654626079b77ef7e63
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2017-03-08 20:25:55 -0800

    added routes for heroku deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcomes#index'
 
+resources :welcomes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
