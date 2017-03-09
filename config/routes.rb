6ecc43e0295bb29a524c9bd25605544c998a3693
nordgaardgit config --global user.email “jenordgaard@gmail.com <jenordgaard@gmail.com>
2017-03-08 21:23:05 -0800

    changed the name of the app

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'welcomes#index'
-
-resources :welcomes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
-
+  resources :welcomes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
